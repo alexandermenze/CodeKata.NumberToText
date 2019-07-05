b427f3d8518529bc2d4fd40330ac8ceeacd5e883
Alexander Menze <a.menze@polipol.de>
2019-07-05 11:26:58 +0200

    Added basic project setup

---
+++ NumberUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeKata.NumbersToText
+{
+    public static class NumberUtil
+    {
+
+        public static string ToText(int number)
+        {
+            if (number < 0 || number > 9999)
+                throw new ArgumentOutOfRangeException(nameof(number), number, "min is 0 and max is 9999");
+
+            return InternalToText(number);
+        }
+
+        private static string InternalToText(int number)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeKata.NumbersToText
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            
+        }
+    }
+}
