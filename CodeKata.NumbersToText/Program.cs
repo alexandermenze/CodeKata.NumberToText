42342f678ea933c2390c32dbd0ba0530891e87ad
Alexander Menze <a.menze@polipol.de>
2019-07-05 11:52:45 +0200

    Added basic input mechanic

--- CodeKata.NumbersToText/Program.cs
+++ CodeKata.NumbersToText/Program.cs
@@ -4,10 +4,33 @@ namespace CodeKata.NumbersToText
 {
     internal class Program
     {
+        private const string ExitSymbol = "e";
+
         internal static void Main(string[] args)
         {
-            Console.WriteLine(NumberUtil.ToText(1735));
-            Console.ReadLine();
+            var input = GetInput();
+
+            while (string.Compare(ExitSymbol, input, StringComparison.OrdinalIgnoreCase) != 0)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(NumberUtil.ToText(int.Parse(input)));
+                }
+                catch (Exception ex) when (ex is ArgumentException || ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine();
+                input = GetInput();
+            }
+        }
+
+        private static string GetInput()
+        {
+            Console.Write("Gib eine Zahl ein: ");
+            return Console.ReadLine();
         }
     }
 }
