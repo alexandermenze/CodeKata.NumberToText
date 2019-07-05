021a1de2c1be4d576efc5d14ee96fbc90d48e352
Alexander Menze <a.menze@polipol.de>
2019-07-05 11:34:24 +0200

    Added basic test code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace CodeKata.NumbersToText
+﻿using System;
+
+namespace CodeKata.NumbersToText
 {
     internal class Program
     {
         internal static void Main(string[] args)
         {
-            
+            Console.WriteLine(NumberUtil.ToText(1735));
+            Console.ReadLine();
         }
     }
 }
